d87c46b85f4eb5055d1ab1428bd3e35e696289f7
Xillians <sandra.toftemo@payex.com>
2022-07-06 17:59:40 +0200

    dynamic sidebar started

--- src/components/Molecules/Instruments.vue
+++ src/components/Molecules/Instruments.vue
@@ -1,26 +1,37 @@
 <template>
   <div class="container-fluid" v-if="loggedIn">
     <div class="row">
-      <div id="sidebar-instruments" class="sidebar">
-        <nav class="side-bar-main-nav">
-          <img src="https://cdn-icons-png.flaticon.com/128/5359/5359828.png" />
-        </nav>
-        <nav class="side-bar-main-nav">
-          <img src="https://cdn-icons-png.flaticon.com/128/5359/5359828.png" />
-        </nav>
-        <nav class="side-bar-main-nav">
-          <img src="https://cdn-icons-png.flaticon.com/128/5359/5359828.png" />
-        </nav>
-      </div>
+      <nav id="sidebar-instruments" class="sidebar side-bar-main-nav">
+        <ul class="list">
+          <li
+            v-for="(instrument, idx) in payment_li_class_list"
+            :key="idx"
+            id="instrument-list"
+          >
+            <i :class="instrument" aria-hidden="true"></i>
+          </li>
+        </ul>
+      </nav>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
+const class_template = "payment-icon payment-icon-";
 export default defineComponent({
   name: "Sidebar-Instruments",
   props: ["loggedIn"],
+  data() {
+    return {
+      payment_li_class_list: [
+        `${class_template}vipps`,
+        `${class_template}swish`,
+        `${class_template}mobilepay`,
+        `${class_template}trustly`,
+        `${class_template}applepay`,
+      ],
+    };
+  },
 });
 </script>
