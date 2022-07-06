70b0f9e5e20e3cf4595d9922603f429660f1abe4
Xillians <sandra.toftemo@payex.com>
2022-07-06 16:38:38 +0200

    sidebar for instrument with example started

---
+++ src/components/Molecules/Instruments.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-fluid" v-if="loggedIn">
+    <div class="row">
+      <div id="sidebar-instruments" class="sidebar">
+        <nav class="side-bar-main-nav">
+          <img src="https://cdn-icons-png.flaticon.com/128/5359/5359828.png" />
+        </nav>
+        <nav class="side-bar-main-nav">
+          <img src="https://cdn-icons-png.flaticon.com/128/5359/5359828.png" />
+        </nav>
+        <nav class="side-bar-main-nav">
+          <img src="https://cdn-icons-png.flaticon.com/128/5359/5359828.png" />
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Sidebar-Instruments",
+  props: ["loggedIn"],
+});
+</script>
