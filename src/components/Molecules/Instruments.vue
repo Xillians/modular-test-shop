468309e4ceca7745007d5e97bfeeedce9f35b27d
Xillians <chkxillians@gmail.com>
2022-07-08 13:29:55 +0200

    navigation bar updated to use navbar component

--- src/components/Molecules/Instruments.vue
+++ src/components/Molecules/Instruments.vue
@@ -1,28 +1,35 @@
 <template>
-  <div class="container-fluid" v-if="loggedIn">
+  <div class="container-fluid">
     <div class="row">
-      <nav id="sidebar-instruments" class="sidebar side-bar-main-nav">
-        <ul class="list">
-          <li
-            v-for="(instrument, idx) in payment_li_class_list"
-            :key="idx"
-            id="instrument-list"
-          >
-            <i :class="instrument" aria-hidden="true"></i>
-          </li>
-        </ul>
-      </nav>
+      <div id="dg-sidebar" class="sidebar">
+        <nav class="sidebar-main-nav">
+          <ul class="main-nav-ul">
+            <li class="main-nav-li active">
+              <a href="#">
+                <i class="material-icons-outlined" aria-hidden="true">home</i>
+                Home Page
+              </a>
+            </li>
+            <sidebar-nav-element/>
+          </ul>
+        </nav>
+      </div>
+      <main class="col main-content">Main content things...</main>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import SidebarNavElement from '../Atoms/SidebarNavElement.vue';
 const class_template = "payment-icon payment-icon-huge payment-icon-";
 const dg = require("@swedbankpay/design-guide");
 
 export default defineComponent({
   name: "Sidebar-Instruments",
+  components: {
+    SidebarNavElement
+  },
   data() {
     return {
       payment_li_class_list: [
