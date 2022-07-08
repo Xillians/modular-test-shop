3162e00e92b75124e18d42de19ed8ae23ac92402
Xillians <chkxillians@gmail.com>
2022-07-08 11:27:26 +0200

    lint fix

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -5,46 +5,44 @@ describe("Login.vue", () => {
   const userList = [
     {
       username: "asd",
-      password: "asdasd"
+      password: "asdasd",
     },
     {
       username: "asd",
-      password: "asdasd"
+      password: "asdasd",
     },
     {
       username: "asd",
-      password: "asdasd"
+      password: "asdasd",
     },
     {
       username: "asd",
-      password: "asdasd"
+      password: "asdasd",
     },
     {
       username: "asd",
-      password: "asdasd"
+      password: "asdasd",
     },
-  ]
+  ];
   it("Username is rendered correctly", () => {
     const wrapper = mount(Login);
-    const email = wrapper.find('Email');
+    const email = wrapper.find("Email");
 
     expect(email).not.toBeUndefined();
   });
   it("Password is rendered correctly", () => {
     const wrapper = mount(Login);
-    const password_input = wrapper.find('password');
+    const password_input = wrapper.find("password");
 
     expect(password_input).not.toBeUndefined();
   });
   it("Login button is rendered correctly", () => {
     const wrapper = mount(Login);
-    const button = wrapper.find('log-in-button');
+    const button = wrapper.find("log-in-button");
 
     expect(button).not.toBeUndefined();
   });
   it.todo("complete a login flow", () => {});
   it.skip("fail to log in and error happens", () => {});
-  it.each(userList)("all users log in successfully", (user) => {
-    
-  });
+  it.each(userList)("all users log in successfully", (user) => {});
 });
